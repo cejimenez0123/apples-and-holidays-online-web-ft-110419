7b749d8a58399d533a4b077f22190014839a2dfd
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-14 01:47:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,8 +77,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_bbq = []
  holiday_hash.each do |season,holiday|
-   binding.pry
-   holiday.each do |item| if item == "BBQ"
+    holiday.each do |item| if item == "BBQ"
+     binding.pry
    holiday_bbq.push(holiday) end
  end
  end
