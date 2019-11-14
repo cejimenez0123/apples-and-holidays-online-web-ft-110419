f98754abae3a15720c88c7b0605243fe3bd71d32
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-14 01:46:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ puts "  #{festivals.join(" ")}: #{items.join(", ")}"
 end
 end
 end
-require "Pry"
+require "pry"
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
